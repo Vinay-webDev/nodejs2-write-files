cf3c9d27fb66c5c2df6a6ec456c87cc55b686a65
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-05 07:16:46 +0530

    3. uncaughtException, process.on( )
    uncaught error
    process which is available by node itself
    no need to import process module*****

--- index.js
+++ index.js
@@ -2,20 +2,33 @@
 // to read file
 
 const fs = require('fs');
-
+/*
 fs.readFile('./files/hi.txt', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
+// buffer data to string
 fs.readFile('./files/hi.txt', (err, data) => {
     if (err) throw err;
     console.log(data.toString());
 });
+// another method buffer data ==>> string
 fs.readFile('./files/hi.txt' , 'utf8', (err, data) => {
     if(err) throw err;
     console.log(data);
-});
+}); */
+// what if we have an uncaught error
+// if error is present then according the node fs documentation
+// process is available within node itself we don't need to export it
+fs.readFile('./files/hello.txt', 'utf8' , (err, data) => {
+    if (err) throw err;
+    console.log(data);
+})
 
+process.on('uncaughtException', err => {
+    console.error(`there was an uncaught error: ${err}`);
+    process.exit(1);
+});
 
 
 
