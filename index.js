ef8a7d9d60aec2a0341965023e86f30e20a2e87e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-05 14:39:41 +0530

    fs.writeFile( )

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ fs.readFile('./files/hi.txt' , 'utf8', (err, data) => {
     console.log(data);
 }); */
 // instead of just hard coding the file path we have an handy method**
+/*
 const fs = require('fs');
 const path = require('path');
 
 fs.readFile(path.join(__dirname, 'files', 'hi.txt'), 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
-})
+})  */
 // what if we have an uncaught error
 // if error is present then according the node fs documentation
 // process is available within node itself we don't need to export it
@@ -39,8 +40,15 @@ process.on('uncaughtException', err => {
     process.exit(1);
 });
 */
-
-
+//==========================================================//
+// how write files 
+const fs = require('fs');
+const path = require('path');
+// here we don't need to have 'utf8' which is provided default in that place we need to have the content of the file like text content...etc and here we only (err) no (data) parameter here ofcourse we're only writing file
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Hello, Nice to meet you', (err) => {
+    if (err) throw err;
+    console.log("writing file complete!");
+})
 
 
 
